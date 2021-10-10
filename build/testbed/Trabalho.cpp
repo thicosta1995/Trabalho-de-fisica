f8db4d103b2d1e9c7e1341e0fe8656ceefc4e9be
Thiago Oliveira Costa <thicosta1995@gmail.com>
2021-10-10 18:29:59 -0300

    vamo

---
+++ build/testbed/Trabalho.cpp
@@ -0,0 +1,30 @@
+﻿#include "test.h"
+
+class Trabalho : public Test //voc� cria a sua classe derivada da classe base Test
+{
+public:
+	Trabalho() {
+		// Aqui no construtor voc� inicializa a cena
+
+	}
+
+	void Step(Settings& settings) override
+	{
+		//Chama o passo da simula��o e o algoritmo de rendering
+		Test::Step(settings);
+
+		//show some text in the main screen
+		g_debugDraw.DrawString(5, m_textLine, "Este e' um template para os exercicios!! :)");
+		m_textLine += 15;
+	}
+
+	static Test* Create()  //a classe Test que instancia um objeto da sua nova classe
+						   //o autor da Box2D usa um padr�o de projeto chamado Factory
+						   //para sua arquitetura de classes
+	{
+		return new Trabalho;
+	}
+};
+
+//Aqui fazemos o registro do novo teste 
+static int testIndex = RegisterTest("Examples", "MyTest", Trabalho::Create);
